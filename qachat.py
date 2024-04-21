6b4753fdc9e6e113b649958d0c2c376e50e3ab81
Tushar Chandra <64960756+me-tusharchandra@users.noreply.github.com>
2024-04-21 15:46:08 +0530

    reqs

--- qachat.py
+++ qachat.py
@@ -11,7 +11,7 @@ from transformers import pipeline
 import google.generativeai as genai
 from langchain_community.vectorstores import Chroma
 from sentence_transformers import SentenceTransformer
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 
